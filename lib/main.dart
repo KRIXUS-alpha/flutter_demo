af064a6c23a443e8319b86f88d5648fe19d5c03d
KRIXUS-alpha <kartikrockson1@gmail.com>
2022-02-03 12:39:49 +0530

    Custom sytling

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,23 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
       home: Scaffold(
-        appBar:AppBar(
-          title: const Text("app bar"),
+    appBar: AppBar(
+      title: const Text("app bar"),
+      centerTitle: true,
+      backgroundColor: Colors.lightBlue,
+    ),
+    body: const Center(child: Text("hello world",
+    style: TextStyle(
+      fontWeight: FontWeight.bold,
+      color: Colors.grey,
+      letterSpacing: 2.0
+
+    ),)),
+    floatingActionButton:
+        FloatingActionButton(
+            onPressed: () {},
+            child: Text("click"),
+          backgroundColor: Colors.lightGreen,
         ),
-      )
-  ));
+  )));
 }
